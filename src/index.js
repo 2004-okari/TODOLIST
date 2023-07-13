e4e2b63f9bfec830e55004a4577922b33fd81fc0
okari <nyandika15@gmail.com>
2023-07-13 17:00:20 +0300

    correct js order

--- src/index.js
+++ src/index.js
@@ -3,6 +3,44 @@ import './style.css';
 const taskInput = document.querySelector('.list-item');
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
+// Function to render tasks in the DOM
+function renderTasks() {
+  const listStorage = document.querySelector('.list-storage');
+  listStorage.innerHTML = '';
+
+  tasks.forEach((task, index) => {
+    const toDoList = document.createElement('div');
+    toDoList.classList.add('item', `item-${index}`);
+    toDoList.innerHTML = `
+      <div class="label">
+        <input type="checkbox" class="checkbox">
+        <input type="text" name="name" value="${task.description}" readonly class="text-center">
+      </div>
+      <i class="fa-solid fa-trash-can icon-3 btn-${index}" data-index="${index}"></i>`;
+
+    listStorage.appendChild(toDoList);
+  });
+
+  // Attach event listeners to remove buttons
+  const removeButtons = document.querySelectorAll('.icon-3');
+  removeButtons.forEach((removeButton, index) => {
+    // eslint-disable-next-line no-use-before-define
+    removeButton.addEventListener('click', () => removeTask(index));
+  });
+
+  // Attach event listeners to text inputs for editing
+  const textCenterInputs = document.querySelectorAll('.text-center');
+  textCenterInputs.forEach((input) => {
+    input.addEventListener('click', () => {
+      if (input.hasAttribute('readonly')) {
+        input.removeAttribute('readonly');
+      } else {
+        input.setAttribute('readonly', 'true');
+      }
+    });
+  });
+}
+
 // Use info from input to add to list
 const addTask = () => {
   if (taskInput.value) {
@@ -15,8 +53,9 @@ const addTask = () => {
   }
 
   localStorage.setItem('tasks', JSON.stringify(tasks));
-
   taskInput.value = '';
+
+  renderTasks(); // Update the DOM
 
   return tasks;
 };
@@ -24,56 +63,21 @@ const addTask = () => {
 const createTask = document.querySelector('.icon-2');
 createTask.addEventListener('click', addTask);
 
-// Displaying book
-let i = 0;
-
-const listStorage = document.querySelector('.list-storage');
-
-// eslint-disable-next-line array-callback-return
-tasks.map((task, index) => {
-  const toDoList = document.createElement('div');
-  toDoList.classList.add('item', `item-${i}`);
-  toDoList.innerHTML = `
-    <div class="label">
-      <input type="checkbox" class="checkbox">
-      <input type="text" name="name" value="${task.description}" readonly class="text-center">
-    </div>
-    <i class="fa-solid fa-trash-can icon-3 btn-${i}" data-index="${index}"></i>`;
-
-  listStorage.appendChild(toDoList);
-  i += 1;
-}).join('');
-
-// Update index list
-const decreaseIndexValue = (startIndex, tasks) => {
-  for (let i = startIndex; i < tasks.length; i += 1) {
-    tasks[i].index -= 1;
-  }
-};
-
 // Remove task function
 function removeTask(index) {
   tasks.splice(index, 1);
-  decreaseIndexValue(index, tasks);
+
+  // Update index list
+  for (let i = index; i < tasks.length; i += 1) {
+    tasks[i].index -= 1;
+  }
+
   localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  renderTasks(); // Update the DOM
+
   return tasks;
 }
 
-// Attach event listener to remove buttons
-const removeButtons = document.querySelectorAll('.icon-3');
-removeButtons.forEach((removeButton, index) => {
-  removeButton.addEventListener('click', () => removeTask(index));
-});
-
-// Editing task
-const textCenterInputs = document.querySelectorAll('.text-center');
-
-textCenterInputs.forEach((input) => {
-  input.addEventListener('click', () => {
-    if (input.hasAttribute('readonly')) {
-      input.removeAttribute('readonly');
-    } else {
-      input.setAttribute('readonly', 'true');
-    }
-  });
-});
+// Load and render tasks on initial page load
+renderTasks();
