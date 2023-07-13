4b69923db9c4e818f140841cdfa0418edce6b266
okari <nyandika15@gmail.com>
2023-07-13 13:22:12 +0300

    js solution

--- src/index.js
+++ src/index.js
@@ -28,6 +28,8 @@ createTask.addEventListener('click', addTask);
 let i = 0;
 
 const listStorage = document.querySelector('.list-storage');
+
+// eslint-disable-next-line array-callback-return
 tasks.map((task, index) => {
   const toDoList = document.createElement('div');
   toDoList.classList.add('item', `item-${i}`);
@@ -38,41 +40,41 @@ tasks.map((task, index) => {
     </div>
     <i class="fa-solid fa-trash-can icon-3 btn-${i}" data-index="${index}"></i>`;
 
-    listStorage.appendChild(toDoList);
-    i += 1;
+  listStorage.appendChild(toDoList);
+  i += 1;
+}).join('');
 
-  }).join('');
-  
-  // Attach event listener to remove buttons
-  const removeButtons = document.querySelectorAll('.icon-3');
-  removeButtons.forEach(removeButton => {
-    const index = removeButton.dataset.index;
-    removeButton.addEventListener('click', () => removeTask(index));
-  });
-  //Update index list
-  const decreaseIndexValue = (tasks) => {
-    tasks.forEach(task => {
-      task.index -= 1;
-    });
+// Update index list
+const decreaseIndexValue = (startIndex, tasks) => {
+  for (let i = startIndex; i < tasks.length; i += 1) {
+    tasks[i].index -= 1;
   }
-  
-  // Remove task function
-  function removeTask(index) {
-    tasks.splice(index, 1);
-    decreaseIndexValue(tasks)
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    return tasks;
-  }
-  
-  
-  //Editing task 
+};
+
+// Remove task function
+function removeTask(index) {
+  tasks.splice(index, 1);
+  decreaseIndexValue(index, tasks);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  return tasks;
+}
+
+// Attach event listener to remove buttons
+const removeButtons = document.querySelectorAll('.icon-3');
+removeButtons.forEach((removeButton) => {
+  const { index } = removeButton.dataset;
+  removeButton.addEventListener('click', () => removeTask(index));
+});
+
+// Editing task
 const textCenterInputs = document.querySelectorAll('.text-center');
 
-textCenterInputs.forEach(input => {
+textCenterInputs.forEach((input) => {
   input.addEventListener('click', () => {
     if (input.hasAttribute('readonly')) {
       input.removeAttribute('readonly');
     } else {
       input.setAttribute('readonly', 'true');
-    }  });
+    }
+  });
 });
